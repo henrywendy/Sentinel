4feb5b71b68c054c9bf61789424bdc8970ab8aed
henry_chen <chenjiegen@eqxiu.com>
2018-12-25 20:04:08 +0800

    add zk

---
+++ sentinel-dashboard/src/main/java/com/taobao/csp/sentinel/dashboard/repository/rule/ZkFlowRule/FlowRuleZkPublisher.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2015 eqxiu.com 北京中网易企秀科技有限公司  All rights reserved.
+ */
+package com.taobao.csp.sentinel.dashboard.repository.rule.ZkFlowRule;
+
+/**
+ * @description //TODO 设计说明
+ * @author henry_chen
+ * @date 2018年12月25日
+ */
+public class FlowRuleZkPublisher {
+
+}
---
+++ sentinel-dashboard/src/main/java/com/taobao/csp/sentinel/dashboard/repository/rule/ZkFlowRule/ZkClientHelper.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright © 2015 eqxiu.com 北京中网易企秀科技有限公司  All rights reserved.
+ */
+package com.taobao.csp.sentinel.dashboard.repository.rule.ZkFlowRule;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description //TODO 设计说明
+ * @author henry_chen
+ * @date 2018年12月25日
+ */
+@Component
+public class ZkClientHelper {
+
+    private String connectAddr;
+
+    private String sessionTimeOut;
+
+    private String retryTime = "10";//默认重试10次
+
+    private String retryTimeGap = "1000";//毫秒
+
+    public CuratorFramework buildClient() {
+        RetryPolicy policy = new ExponentialBackoffRetry(Integer.valueOf(this.retryTimeGap), Integer.valueOf(this.retryTime));
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString(this.connectAddr).sessionTimeoutMs(Integer.valueOf(this.sessionTimeOut)).retryPolicy(policy).build();
+        client.start();
+        return client;
+    }
+}
